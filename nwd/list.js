a220c742a26ca533f962da074dda6de71f4d4f9e
Koralkov Artem <artemkoralkov@gmail.com>
2022-06-12 19:13:57 +0300

    fix: fix list all files function

--- nwd/list.js
+++ nwd/list.js
@@ -4,7 +4,7 @@ export const list = async (path) => {
     // Write your code here 
     promises.readdir(path)
     .then((files) => {
-        console.log(files);
+       console.dir(files, {'maxArrayLength': null});
     })
     .catch((err) => {
        console.log('Operation Failed');
